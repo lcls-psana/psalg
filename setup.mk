7ab332ae0aad2566360e6b763df49bfa5fe6242c
Daniel Damiani <ddamiani@slac.stanford.edu>
2024-06-16 18:55:40 +0000

    add rhel9/rocky9 build target

--- setup.mk
+++ setup.mk
@@ -1,4 +1,4 @@
-arch_size = i386-linux x86_64-linux x86_64-rhel6 x86_64-rhel7
+arch_size = i386-linux x86_64-linux x86_64-rhel6 x86_64-rhel7 x86_64-rhel9
 arch_opts = -opt -dbg -opt-dbg -dbg-opt
 
 archs := $(foreach aopt,$(arch_opts),$(addsuffix $(aopt),$(arch_size)))
